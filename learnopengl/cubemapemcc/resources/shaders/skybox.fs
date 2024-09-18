29f9c3ee64ad2bf9f825d76cbcda82316bd84de2
tom <tomlikesnakes@tutamail.com>
2024-09-18 18:00:07 -0400

    commit shaders

---
+++ learnopengl/cubemapemcc/resources/shaders/skybox.fs
@@ -0,0 +1,12 @@
+#version 300 es
+precision mediump float;
+
+in vec3 TexCoords;
+out vec4 FragColor;
+
+uniform samplerCube skybox;
+
+void main()
+{    
+    FragColor = texture(skybox, TexCoords);
+}
