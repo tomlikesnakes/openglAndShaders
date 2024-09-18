c7bd74b76146cb16da80a4bbdaff8ef4e391e15f
tom <tomlikesnakes@tutamail.com>
2024-09-18 19:57:31 -0400

    test.c kind of work, test2.c not, problems with Drawing it says error clientside on debug console js

--- learnopengl/cubemapemcc/test.c
+++ learnopengl/cubemapemcc/test.c
@@ -1,66 +1,144 @@
 #include "raylib.h"
+#include "raymath.h"  // Make sure to include this for matrix operations
 #include "rlgl.h"
 #include <emscripten/emscripten.h>
+#include <GLES3/gl3.h>
+#include <stdio.h>
+#include <math.h>
 
 #define GLSL_VERSION 300
 
 // Global variables
 Camera camera = { 0 };
 Model skybox;
+Shader skyboxShader;
+unsigned int skyboxVAO = 0;
+int frameCount = 0;
+
 
 // Function prototypes
 void UpdateDrawFrame(void);
 void InitGame(void);
+void LogError(const char* message);
+void LogInfo(const char* message);
+void CheckGLError(const char* operation);
+
+// Function to create perspective projection matrix
+Matrix MatrixPerspectiveCustom(double fovY, double aspect, double nearPlane, double farPlane)
+{
+    Matrix result = { 0 };
+    double f = 1.0 / tan(fovY * 0.5);
+    
+    result.m0 = f / aspect;
+    result.m5 = f;
+    result.m10 = (farPlane + nearPlane) / (nearPlane - farPlane);
+    result.m11 = -1.0f;
+    result.m14 = (2.0 * farPlane * nearPlane) / (nearPlane - farPlane);
+    
+    return result;
+}
 
 int main(void)
 {
     const int screenWidth = 800;
     const int screenHeight = 450;
 
+    SetTraceLogLevel(LOG_DEBUG);
+    LogInfo("Initializing window...");
+    SetConfigFlags(FLAG_MSAA_4X_HINT);
     InitWindow(screenWidth, screenHeight, "Emscripten Cubemap Skybox");
     
+    if (!IsWindowReady()) {
+        LogError("Failed to create window or OpenGL context");
+        return 1;
+    }
+    LogInfo("Window initialized successfully");
+
     InitGame();
 
+    LogInfo("Setting up main loop...");
     emscripten_set_main_loop(UpdateDrawFrame, 0, 1);
 
-    UnloadShader(skybox.materials[0].shader);
+    LogInfo("Cleaning up resources...");
+    UnloadShader(skyboxShader);
     UnloadTexture(skybox.materials[0].maps[MATERIAL_MAP_CUBEMAP].texture);
     UnloadModel(skybox);
 
     CloseWindow();
+    LogInfo("Application closed successfully");
 
     return 0;
 }
 
 void InitGame(void)
 {
+    LogInfo("Initializing game...");
+    
     // Initialize camera
     camera.position = (Vector3){ 0.0f, 0.0f, 0.0f };
     camera.target = (Vector3){ 0.0f, 0.0f, 1.0f };
     camera.up = (Vector3){ 0.0f, 1.0f, 0.0f };
     camera.fovy = 90.0f;
     camera.projection = CAMERA_PERSPECTIVE;
+    LogInfo("Camera initialized");
 
     // Create skybox
+    LogInfo("Creating skybox mesh...");
     Mesh cube = GenMeshCube(1.0f, 1.0f, 1.0f);
+    if (cube.vertexCount == 0) {
+        LogError("Failed to generate cube mesh");
+        return;
+    }
     skybox = LoadModelFromMesh(cube);
+    LogInfo("Skybox mesh created successfully");
 
-    // Load and compile shaders
-    skybox.materials[0].shader = LoadShader("resources/shaders/skybox.vs", "resources/shaders/skybox.fs");
-    SetShaderValue(skybox.materials[0].shader, GetShaderLocation(skybox.materials[0].shader, "environmentMap"), (int[1]){ MATERIAL_MAP_CUBEMAP }, SHADER_UNIFORM_INT);
+        // Load and compile shaders
+    LogInfo("Loading shaders...");
+    skyboxShader = LoadShader("resources/shaders/skybox.vs", "resources/shaders/skybox.fs");
+    if (!IsShaderReady(skyboxShader)) {
+        LogError("Failed to load skybox shader");
+        return;
+    }
 
-    Image faces[6] = {
-        LoadImage("resources/textures/skybox/right.png"),
-        LoadImage("resources/textures/skybox/left.png"),
-        LoadImage("resources/textures/skybox/top.png"),
-        LoadImage("resources/textures/skybox/bottom.png"),
-        LoadImage("resources/textures/skybox/front.png"),
-        LoadImage("resources/textures/skybox/back.png")
+    int projectionLoc = GetShaderLocation(skyboxShader, "projection");
+    int viewLoc = GetShaderLocation(skyboxShader, "view");
+    int skyboxLoc = GetShaderLocation(skyboxShader, "skybox");
+    
+    if (projectionLoc == -1) LogError("Failed to locate 'projection' uniform");
+    if (viewLoc == -1) LogError("Failed to locate 'view' uniform");
+    if (skyboxLoc == -1) LogError("Failed to locate 'skybox' uniform");
+
+    // Set the skybox uniform to use texture unit 0
+    SetShaderValue(skyboxShader, skyboxLoc, (int[1]){ 0 }, SHADER_UNIFORM_INT);
+    
+    LogInfo("Shaders loaded successfully");
+
+    LogInfo("Loading skybox textures...");
+    Image faces[6];
+    const char* faceFiles[] = {
+        "resources/textures/skybox/right.png",
+        "resources/textures/skybox/left.png",
+        "resources/textures/skybox/top.png",
+        "resources/textures/skybox/bottom.png",
+        "resources/textures/skybox/front.png",
+        "resources/textures/skybox/back.png"
     };
+
+    for (int i = 0; i < 6; i++) {
+        faces[i] = LoadImage(faceFiles[i]);
+        if (!IsImageReady(faces[i])) {
+            LogError("Failed to load skybox texture");
+            return;
+        }
+    }
 
     int faceWidth = faces[0].width;
     int faceHeight = faces[0].height;
     Image verticalImage = GenImageColor(faceWidth, faceHeight * 6, BLANK);
+    if (!IsImageReady(verticalImage)) {
+        LogError("Failed to generate vertical image for cubemap");
+        return;
+    }
 
     for (int i = 0; i < 6; i++) {
         ImageDraw(&verticalImage, faces[i], (Rectangle){0, 0, faceWidth, faceHeight}, (Rectangle){0, i * faceHeight, faceWidth, faceHeight}, WHITE);
@@ -68,19 +146,114 @@ void InitGame(void)
     }
 
     skybox.materials[0].maps[MATERIAL_MAP_CUBEMAP].texture = LoadTextureCubemap(verticalImage, CUBEMAP_LAYOUT_LINE_VERTICAL);
+    if (!IsTextureReady(skybox.materials[0].maps[MATERIAL_MAP_CUBEMAP].texture)) {
+        LogError("Failed to load cubemap texture");
+        return;
+    }
     UnloadImage(verticalImage);
+    LogInfo("Skybox textures loaded successfully");
+
+        // Create VAO for skybox using Raylib functions
+    LogInfo("Creating VAO for skybox...");
+    skyboxVAO = rlLoadVertexArray();
+    rlEnableVertexArray(skyboxVAO);
+
+    unsigned int vbo = rlLoadVertexBuffer(skybox.meshes[0].vertices, skybox.meshes[0].vertexCount * 3 * sizeof(float), false);
+    rlSetVertexAttribute(0, 3, RL_FLOAT, false, 0, 0);
+    rlEnableVertexAttribute(0);
+
+    rlDisableVertexArray();
+    LogInfo("VAO created successfully");
+
+    LogInfo("Game initialization completed");
 }
 
 void UpdateDrawFrame(void)
 {
+    frameCount++;
+    if (frameCount % 60 == 0) {
+        LogInfo(TextFormat("Frame %d", frameCount));
+    }
+
+    LogInfo("A");
     BeginDrawing();
-        ClearBackground(RAYWHITE);
-        BeginMode3D(camera);
-            rlDisableBackfaceCulling();
-            rlDisableDepthMask();
-            DrawModel(skybox, (Vector3){0, 0, 0}, 1.0f, WHITE);
-            rlEnableBackfaceCulling();
-            rlEnableDepthMask();
-        EndMode3D();
+    ClearBackground(RAYWHITE);
+    
+    LogInfo("B");
+
+    BeginMode3D(camera);
+    
+    LogInfo("C");
+    // Use Raylib's functions instead of direct OpenGL calls
+    rlDisableBackfaceCulling();
+    rlDisableDepthTest();
+    
+    LogInfo("D");
+    // Use the skybox shader
+    rlEnableShader(skyboxShader.id);
+    
+    // Bind the cubemap texture
+    rlActiveTextureSlot(0);
+    rlEnableTextureCubemap(skybox.materials[0].maps[MATERIAL_MAP_CUBEMAP].texture.id);
+    
+    LogInfo("E");
+    // Set uniforms
+    float aspect = (float)GetScreenWidth() / (float)GetScreenHeight();
+    Matrix projection = MatrixPerspective(camera.fovy * DEG2RAD, aspect, 0.01f, 1000.0f);
+    Matrix view = GetCameraMatrix(camera);
+    view.m12 = 0.0f; view.m13 = 0.0f; view.m14 = 0.0f; // Remove translation
+
+    rlSetUniformMatrix(GetShaderLocation(skyboxShader, "projection"), projection);
+    rlSetUniformMatrix(GetShaderLocation(skyboxShader, "view"), view);
+    rlSetUniform(GetShaderLocation(skyboxShader, "skybox"), (int[1]){0}, SHADER_UNIFORM_INT, 1);
+    
+    LogInfo("F");
+    // Draw the skybox
+    rlEnableVertexArray(skyboxVAO);
+    rlDrawVertexArray(0, 36);
+    rlDisableVertexArray();
+    
+    LogInfo("G");
+    // Disable the shader and texture
+    rlDisableShader();
+    rlDisableTextureCubemap();
+    
+    // Re-enable depth test and backface culling
+    rlEnableDepthTest();
+    rlEnableBackfaceCulling();
+    
+    LogInfo("H");
+    EndMode3D();
     EndDrawing();
+}
+
+void LogError(const char* message)
+{
+    TraceLog(LOG_ERROR, message);
+    printf("ERROR: %s\n", message);
+}
+
+void LogInfo(const char* message)
+{
+    TraceLog(LOG_INFO, message);
+    printf("INFO: %s\n", message);
+}
+
+void CheckGLError(const char* operation)
+{
+    GLenum error;
+    while ((error = glGetError()) != GL_NO_ERROR)
+    {
+        const char* errorString;
+        switch (error)
+        {
+            case GL_INVALID_ENUM:                  errorString = "INVALID_ENUM"; break;
+            case GL_INVALID_VALUE:                 errorString = "INVALID_VALUE"; break;
+            case GL_INVALID_OPERATION:             errorString = "INVALID_OPERATION"; break;
+            case GL_OUT_OF_MEMORY:                 errorString = "OUT_OF_MEMORY"; break;
+            case GL_INVALID_FRAMEBUFFER_OPERATION: errorString = "INVALID_FRAMEBUFFER_OPERATION"; break;
+            default:                               errorString = "UNKNOWN"; break;
+        }
+        LogError(TextFormat("OpenGL error after %s: %s", operation, errorString));
+    }
 }
---
+++ learnopengl/cubemapemcc/test2.c
@@ -0,0 +1,329 @@
+#include <stdlib.h>
+#include <string.h>
+#include "raylib.h"
+#include "rlgl.h"
+#include "raymath.h"
+#include <stdio.h>
+
+void LogInfo(const char* message);
+void LogError(const char* message);
+#if defined(PLATFORM_WEB)
+    #include <emscripten/emscripten.h>
+    #define GLSL_VERSION            300 es
+#else
+    #define GLSL_VERSION            300 es
+#endif
+
+// Global variables for toggles
+bool showGrid = true;
+bool showFPS = true;
+bool showSkybox = true;
+bool insideSkybox = false;
+
+// Global variables for camera and movement
+Camera camera = { 0 };
+Model skybox;
+float zoom = 1.0f;
+float moveSpeed = 0.1f;
+
+#if defined(PLATFORM_WEB)
+// Global variables for key states (web version)
+bool keyW = false;
+bool keyS = false;
+bool keyA = false;
+bool keyD = false;
+bool keySpace = false;
+bool keyLeftControl = false;
+bool keyUp = false;
+bool keyDown = false;
+bool keyTab = false;
+bool keyG = false;
+bool keyF = false;
+bool keyB = false;
+bool keyI = false;
+bool keyR = false;
+#endif
+
+// Function prototypes
+void UpdateDrawFrame(void);
+void InitGame(void);
+
+#if defined(PLATFORM_WEB)
+EMSCRIPTEN_KEEPALIVE
+void UpdateKeyState(int key, bool isDown) {
+    switch(key) {
+        case 87: keyW = isDown; break;  // W
+        case 83: keyS = isDown; break;  // S
+        case 65: keyA = isDown; break;  // A
+        case 68: keyD = isDown; break;  // D
+        case 32: keySpace = isDown; break;  // Space
+        case 17: keyLeftControl = isDown; break;  // Left Control
+        case 38: keyUp = isDown; break;  // Up Arrow
+        case 40: keyDown = isDown; break;  // Down Arrow
+        case 9: keyTab = isDown; break;  // Tab
+        case 71: keyG = isDown; break;  // G
+        case 70: keyF = isDown; break;  // F
+        case 66: keyB = isDown; break;  // B
+        case 73: keyI = isDown; break;  // I
+        case 82: keyR = isDown; break;  // R
+    }
+}
+
+EMSCRIPTEN_KEEPALIVE
+void UpdateMouseWheel(float delta) {
+    zoom += delta * 0.1f;
+    if (zoom < 0.1f) zoom = 0.1f;
+    if (zoom > 3.0f) zoom = 3.0f;
+    camera.fovy = 45.0f / zoom;
+}
+#endif
+
+int main(void)
+{
+    const int screenWidth = 800;
+    const int screenHeight = 450;
+
+    //SetConfigFlags(FLAG_MSAA_4X_HINT);
+    InitWindow(screenWidth, screenHeight, "raylib [models] example - enhanced skybox with inside view");
+    
+    InitGame();
+
+#if defined(PLATFORM_WEB)
+    emscripten_set_main_loop(UpdateDrawFrame, 0, 1);
+#else
+    SetTargetFPS(60);
+    while (!WindowShouldClose())
+    {
+        UpdateDrawFrame();
+    }
+#endif
+
+    UnloadShader(skybox.materials[0].shader);
+    UnloadTexture(skybox.materials[0].maps[MATERIAL_MAP_CUBEMAP].texture);
+    UnloadModel(skybox);
+
+    CloseWindow();
+
+    return 0;
+}
+
+void InitGame(void)
+{
+    camera.position = (Vector3){ 1.0f, 1.0f, 1.0f };
+    camera.target = (Vector3){ 4.0f, 1.0f, 4.0f };
+    camera.up = (Vector3){ 0.0f, 1.0f, 0.0f };
+    camera.fovy = 45.0f;
+    camera.projection = CAMERA_PERSPECTIVE;
+
+    Mesh cube = GenMeshCube(1.0f, 1.0f, 1.0f);
+    skybox = LoadModelFromMesh(cube);
+
+    const char* vsFileName = "resources/shaders/skybox.vs";
+    const char* fsFileName = "resources/shaders/skybox.fs";
+
+#if defined(PLATFORM_DESKTOP)
+    const char* versionDirective = "#version 300 es\n";
+#elif defined(PLATFORM_WEB)
+    const char* versionDirective = "#version 300 es\n";
+#else
+    const char* versionDirective = "#version 300 es\n";
+#endif
+
+    char* vShaderCode = LoadFileText(vsFileName);
+    char* fShaderCode = LoadFileText(fsFileName);
+
+    if (vShaderCode != NULL && fShaderCode != NULL)
+    {
+        skybox.materials[0].shader = LoadShaderFromMemory(vShaderCode, fShaderCode);
+
+        UnloadFileText(vShaderCode);
+        UnloadFileText(fShaderCode);
+    }
+    else TraceLog(LOG_WARNING, "Failed to load shader files");
+
+
+    SetShaderValue(skybox.materials[0].shader, GetShaderLocation(skybox.materials[0].shader, "environmentMap"), (int[1]){ MATERIAL_MAP_CUBEMAP }, SHADER_UNIFORM_INT);
+
+    Image faces[6] = {
+        LoadImage("resources/textures/skybox/right.png"),
+        LoadImage("resources/textures/skybox/left.png"),
+        LoadImage("resources/textures/skybox/top.png"),
+        LoadImage("resources/textures/skybox/bottom.png"),
+        LoadImage("resources/textures/skybox/front.png"),
+        LoadImage("resources/textures/skybox/back.png")
+    };
+
+    int faceWidth = faces[0].width;
+    int faceHeight = faces[0].height;
+    Image verticalImage = GenImageColor(faceWidth, faceHeight * 6, BLANK);
+
+    for (int i = 0; i < 6; i++) {
+        ImageDraw(&verticalImage, faces[i], (Rectangle){0, 0, faceWidth, faceHeight}, (Rectangle){0, i * faceHeight, faceWidth, faceHeight}, WHITE);
+        UnloadImage(faces[i]);
+    }
+
+    skybox.materials[0].maps[MATERIAL_MAP_CUBEMAP].texture = LoadTextureCubemap(verticalImage, CUBEMAP_LAYOUT_LINE_VERTICAL);
+    UnloadImage(verticalImage);
+
+    DisableCursor();
+}
+
+void UpdateDrawFrame(void)
+{
+#if defined(PLATFORM_WEB)
+    // Web input handling
+    if (keyW) camera.position = Vector3Add(camera.position, Vector3Scale(Vector3Normalize(Vector3Subtract(camera.target, camera.position)), moveSpeed));
+    if (keyS) camera.position = Vector3Subtract(camera.position, Vector3Scale(Vector3Normalize(Vector3Subtract(camera.target, camera.position)), moveSpeed));
+    if (keyA) camera.position = Vector3Subtract(camera.position, Vector3Scale(Vector3Normalize(Vector3CrossProduct(Vector3Subtract(camera.target, camera.position), camera.up)), moveSpeed));
+    if (keyD) camera.position = Vector3Add(camera.position, Vector3Scale(Vector3Normalize(Vector3CrossProduct(Vector3Subtract(camera.target, camera.position), camera.up)), moveSpeed));
+    if (keySpace) camera.position.y += moveSpeed;
+    if (keyLeftControl) camera.position.y -= moveSpeed;
+
+    if (keyUp) moveSpeed *= 1.1f;
+    if (keyDown) moveSpeed /= 1.1f;
+    if (keyTab) {
+        if (IsCursorHidden()) EnableCursor();
+        else DisableCursor();
+        keyTab = false;
+    }
+    if (keyG) { showGrid = !showGrid; keyG = false; }
+    if (keyF) { showFPS = !showFPS; keyF = false; }
+    if (keyB) { showSkybox = !showSkybox; keyB = false; }
+    if (keyI) {
+        insideSkybox = !insideSkybox;
+        if (insideSkybox) camera.position = (Vector3){ 0.0f, 0.0f, 0.0f };
+        else camera.position = (Vector3){ 1.0f, 1.0f, 1.0f };
+        keyI = false;
+    }
+    if (keyR) {
+        camera.position = (Vector3){ 1.0f, 1.0f, 1.0f };
+        camera.target = (Vector3){ 4.0f, 1.0f, 4.0f };
+        zoom = 1.0f;
+        camera.fovy = 45.0f;
+        insideSkybox = false;
+        keyR = false;
+    }
+#else
+    // Native input handling
+    if (IsKeyDown(KEY_W)) camera.position = Vector3Add(camera.position, Vector3Scale(Vector3Normalize(Vector3Subtract(camera.target, camera.position)), moveSpeed));
+    if (IsKeyDown(KEY_S)) camera.position = Vector3Subtract(camera.position, Vector3Scale(Vector3Normalize(Vector3Subtract(camera.target, camera.position)), moveSpeed));
+    if (IsKeyDown(KEY_A)) camera.position = Vector3Subtract(camera.position, Vector3Scale(Vector3Normalize(Vector3CrossProduct(Vector3Subtract(camera.target, camera.position), camera.up)), moveSpeed));
+    if (IsKeyDown(KEY_D)) camera.position = Vector3Add(camera.position, Vector3Scale(Vector3Normalize(Vector3CrossProduct(Vector3Subtract(camera.target, camera.position), camera.up)), moveSpeed));
+    if (IsKeyDown(KEY_SPACE)) camera.position.y += moveSpeed;
+    if (IsKeyDown(KEY_LEFT_CONTROL)) camera.position.y -= moveSpeed;
+
+    float wheel = GetMouseWheelMove();
+    if (wheel != 0) {
+        zoom += wheel * 0.1f;
+        if (zoom < 0.1f) zoom = 0.1f;
+        if (zoom > 3.0f) zoom = 3.0f;
+        camera.fovy = 45.0f / zoom;
+    }
+
+    if (IsKeyPressed(KEY_UP)) moveSpeed *= 1.1f;
+    if (IsKeyPressed(KEY_DOWN)) moveSpeed /= 1.1f;
+    if (IsKeyPressed(KEY_TAB)) {
+        if (IsCursorHidden()) EnableCursor();
+        else DisableCursor();
+    }
+    if (IsKeyPressed(KEY_G)) showGrid = !showGrid;
+    if (IsKeyPressed(KEY_F)) showFPS = !showFPS;
+    if (IsKeyPressed(KEY_B)) showSkybox = !showSkybox;
+
+    if (IsKeyPressed(KEY_I)) {
+        insideSkybox = !insideSkybox;
+        if (insideSkybox) camera.position = (Vector3){ 0.0f, 0.0f, 0.0f };
+        else camera.position = (Vector3){ 1.0f, 1.0f, 1.0f };
+    }
+
+    if (IsKeyPressed(KEY_R)) {
+        camera.position = (Vector3){ 1.0f, 1.0f, 1.0f };
+        camera.target = (Vector3){ 4.0f, 1.0f, 4.0f };
+        zoom = 1.0f;
+        camera.fovy = 45.0f;
+        insideSkybox = false;
+    }
+#endif
+    LogInfo("A");
+    UpdateCamera(&camera, CAMERA_FIRST_PERSON);
+
+    LogInfo("B");
+    BeginDrawing();
+
+    LogInfo("C");
+        ClearBackground(RAYWHITE);
+
+    LogInfo("D");
+        BeginMode3D(camera);
+
+    LogInfo("E");
+            if (showSkybox) {
+    LogInfo("F");
+                rlDisableBackfaceCulling();
+    LogInfo("G");
+                rlDisableDepthMask();
+    LogInfo("H");
+                if (insideSkybox) {
+    LogInfo("I");
+                    rlScalef(500.0f, 500.0f, 500.0f);
+    LogInfo("J");
+                }
+    LogInfo("K");
+                DrawModel(skybox, (Vector3){0, 0, 0}, 1.0f, WHITE);
+    LogInfo("L");
+                rlEnableBackfaceCulling();
+    LogInfo("M");
+                rlEnableDepthMask();
+    LogInfo("N");
+            }
+    LogInfo("O");
+
+    LogInfo("P");
+            if (showGrid && !insideSkybox) DrawGrid(10, 1.0f);
+    LogInfo("Q");
+
+    LogInfo("R");
+        EndMode3D();
+    LogInfo("S");
+
+    LogInfo("T");
+        if (showFPS) DrawFPS(10, 10);
+    LogInfo("U");
+
+    LogInfo("W");
+        DrawText("Controls:", 10, 30, 20, BLACK);
+    LogInfo("W");
+        DrawText("WASD: Move | SPACE/CTRL: Up/Down | TAB: Toggle cursor", 10, 50, 10, DARKGRAY);
+    LogInfo("X");
+        DrawText("G: Toggle grid | F: Toggle FPS | B: Toggle skybox", 10, 70, 10, DARKGRAY);
+    LogInfo("Y");
+        DrawText("R: Reset | UP/DOWN: Adjust speed | Mouse wheel: Zoom", 10, 90, 10, DARKGRAY);
+    LogInfo("A");
+
+    LogInfo("A");
+        DrawText("I: Toggle inside/outside view", 10, 110, 10, DARKGRAY);
+    LogInfo("A");
+
+    LogInfo("A");
+        DrawText(TextFormat("Zoom: %.2fx", zoom), 10, 130, 20, BLACK);
+    LogInfo("A");
+        DrawText(TextFormat("View: %s", insideSkybox ? "Inside" : "Outside"), 10, 150, 20, BLACK);
+    LogInfo("A");
+
+    LogInfo("A");
+    EndDrawing();
+    LogInfo("A");
+}
+
+void LogError(const char* message)
+{
+    TraceLog(LOG_ERROR, message);
+    printf("ERROR: %s\n", message);
+}
+
+void LogInfo(const char* message)
+{
+    TraceLog(LOG_INFO, message);
+    printf("INFO: %s\n", message);
+}
+
